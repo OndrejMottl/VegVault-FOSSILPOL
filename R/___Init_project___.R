22eeeffa3a369a8b8ca1f556c8af0ac1b04c1529
Ondřej Mottl <60345393+OndrejMottl@users.noreply.github.com>
2023-06-26 08:04:34 +0200

    Initial commit

---
+++ R/___Init_project___.R
@@ -0,0 +1,153 @@
+#----------------------------------------------------------#
+#
+#
+#                 The FOSSILPOL workflow
+#
+#                     Project setup
+#
+#
+#   O. Mottl, S. Flantua, K. Bhatta, V. Felde, A. Seddon
+#                         2023
+#
+#----------------------------------------------------------#
+
+# Script to prepare all necessary components of environment to run the Project.
+#   Needs to be run only once
+
+
+#----------------------------------------------------------#
+# Step 0: Define package list and custom function -----
+#----------------------------------------------------------#
+
+# list of all CRAN packages
+package_list <-
+  c(
+    "assertthat",
+    "Bchron",
+    "dplyr",
+    "forcats",
+    "furrr",
+    "future",
+    "ggplot2",
+    "ggpubr",
+    "grDevices",
+    "here",
+    "httr",
+    "IntCal",
+    "janitor",
+    "knitr",
+    "lifecycle",
+    "magrittr",
+    "methods",
+    "purrr",
+    "qs",
+    "raster",
+    "rcarbon",
+    "readr",
+    "readxl",
+    "remotes",
+    "rioja",
+    "rlang",
+    "sf",
+    "sp",
+    "stats",
+    "stringr",
+    "tibble",
+    "tidyr",
+    "tidyselect",
+    "tidyverse",
+    "usethis",
+    "utils",
+    "waldo",
+    "zip"
+  )
+
+# define helper function
+install_packages <-
+  function(pkgs_list) {
+    # install all packages in the lst from CRAN
+    sapply(pkgs_list, utils::install.packages, character.only = TRUE)
+
+    # install RUtilpol from GitHub
+    remotes::install_github(
+      repo = "HOPE-UIB-BIO/R-Utilpol-package",
+      ref = "HEAD",
+      quiet = FALSE,
+      upgrade = "ask"
+    )
+
+    # install neotoma2 from GitHub
+    remotes::install_github(
+      repo = "NeotomaDB/neotoma2",
+      quiet = FALSE,
+      upgrade = "ask"
+    )
+
+    # install RFossilpol from GitHub
+    remotes::install_github(
+      repo = "HOPE-UIB-BIO/R-Fossilpol-package@*release",
+      quiet = FALSE,
+      upgrade = "ask"
+    )
+  }
+
+
+#----------------------------------------------------------#
+# Step 1: Install 'renv' package -----
+#----------------------------------------------------------#
+
+utils::install.packages("renv")
+
+
+#----------------------------------------------------------#
+# Step 2: Deactivate 'renv' package -----
+#----------------------------------------------------------#
+
+# deactivate to make sure that packages are updated on the machine
+renv::deactivate()
+
+
+#----------------------------------------------------------#
+# Step 3: Install packages to the machine
+#----------------------------------------------------------#
+
+install_packages(package_list)
+
+
+#----------------------------------------------------------#
+# Step 4: Activate 'renv' project
+#----------------------------------------------------------#
+
+renv::activate()
+
+
+#----------------------------------------------------------#
+# Step 5: Install packages to the project
+#----------------------------------------------------------#
+
+install_packages(package_list)
+
+
+#----------------------------------------------------------#
+# Step 6: Synchronize package versions with the project
+#----------------------------------------------------------#
+
+library(here)
+
+# if there is no lock file present make a new snapshot
+if (
+  isFALSE("library_list.lock" %in% list.files(here::here("renv")))
+) {
+  renv::snapshot(
+    lockfile = here::here("renv/library_list.lock")
+  )
+} else {
+  renv::restore(
+    lockfile = here::here("renv/library_list.lock")
+  )
+}
+
+
+#----------------------------------------------------------#
+# Step 7: Run the project
+#----------------------------------------------------------#
